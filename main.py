10e320328c61327a543022caa0e8c0c5a86b0756
ZaherAmasha <amashaazaherr@gmail.com>
2024-11-10 11:28:52 +0200

    Add basic error handling to the trigger endpoint

--- main.py
+++ main.py
@@ -23,24 +23,27 @@ async def read_root():
 async def update_recommended_products(
     update: SheetUpdate, authorization: str = Header(None)
 ):
-    # Validate the shared secret
-    if authorization != f"Bearer {SHARED_SECRET}":
-        raise HTTPException(status_code=401, detail="Unauthorized")
+    try:
+        # Validate the shared secret
+        if authorization != f"Bearer {SHARED_SECRET}":
+            raise HTTPException(status_code=401, detail="Unauthorized")
 
-    keywords = update.keywords
-    # cell_range = update.range
-    print("keywords: ", keywords)  # , " ", cell_range)
-    for product_order_id, keyword in enumerate(keywords):
-        if fetch_aliexpress_product_recommendations(
-            keyword, product_order_id + 1
-        ):  # the +1 is because it starts from 0
-            print("Fetching products from AliExpress went well")
-            time.sleep(2)
-        else:
-            print("Something went bad when fetching products")
-            return "Something went bad when fetching products"
+        keywords = update.keywords
+        # cell_range = update.range
+        print("keywords: ", keywords)  # , " ", cell_range)
+        for product_order_id, keyword in enumerate(keywords):
+            if fetch_aliexpress_product_recommendations(
+                keyword, product_order_id + 1
+            ):  # the +1 is because it starts from 0
+                print("Fetching products from AliExpress went well")
+                time.sleep(2)
+            else:
+                print("Something went bad when fetching products")
+                return "Something went bad when fetching products"
 
-    return "Fetching products from AliExpress went well"
+        return "Fetching products from AliExpress went well"
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"This is the exception: {e}")
 
 
 print("Launched the server")
