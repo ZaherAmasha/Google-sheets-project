13e97af386ee36c19cffb8e28bb546c8b8413535
ZaherAmasha <amashaazaherr@gmail.com>
2024-11-11 00:09:09 +0200

    Edit aliexpress_api.py

--- aliexpress_api.py
+++ aliexpress_api.py
@@ -173,5 +173,5 @@ def fetch_aliexpress_product_recommendations(search_keyword, product_order_id):
 
 # examples:
 # fetch_aliexpress_product_recommendations("black shoes")
-fetch_aliexpress_product_recommendations("white shoes", 1)
+# fetch_aliexpress_product_recommendations("white shoes", 1)
 # fetch_aliexpress_product_recommendations("zzzzzzzzzzzzzzzzzzzzzz")
