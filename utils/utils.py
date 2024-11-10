da94198304d2f0e28b7304ce089edf205f8b9b16
ZaherAmasha <amashaazaherr@gmail.com>
2024-11-11 00:20:39 +0200

    Fix utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,2 @@
 def remove_elements_with_whitespaces_and_empty_from_list(_list):
-    _list = [element for element in _list if element.strip()]
-    return
+    return [element for element in _list if element.strip()]
